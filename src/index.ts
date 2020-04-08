2363b8cb5ae162551508febe4ed073ece1bd3e93
Max Arndt <m.arndt@mozys.de>
2020-04-08 17:46:53 +0200

    implement regex parsing of the HTML response to get style*.css/.js paths

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const { WritableStream } = require('memory-streams');
 const fs = require('fs');
 
 const mergePdfBuffers = (pdfBuffers: Array<Buffer>) => {
-  const outStream = new WritableStream();                                                                                                                                             
+  const outStream = new WritableStream();
   const [firstPdfRStream, ...restPdfRStreams] = pdfBuffers.map(pdfBuffer => new PDFRStreamForBuffer(pdfBuffer));
   const pdfWriter = createWriterToModify(firstPdfRStream, new PDFStreamForResponse(outStream));
 
@@ -14,14 +14,43 @@ const mergePdfBuffers = (pdfBuffers: Array<Buffer>) => {
 
   pdfWriter.end();
   outStream.end();
-  
+
   return outStream.toBuffer();
 };
 
-const safeUrlAppend = (origin: String, path: String, file: String) => {
-  return origin + (path.length > 0 ? '/' : '') + 
-    path.substring(path.startsWith('/') ? 1 : 0, path.length - (path.endsWith('/') ? 1 : 0)) + 
-    '/' + file;
+const getURL = (origin: string, filePath: string) => {
+  return origin + "/" + filePath.substring(filePath.startsWith('/') ? 1 : 0);
+}
+
+const getStylesheetPathFromHTML = (html: string, origin: string) => {
+  let regExp = /(?:|<link.*){1}href="(.*styles.*?\.css){1}"/g;
+  let filePath = "";
+  try {
+    filePath = getFirstCapturingGroup(regExp, html);
+  } catch {
+    throw new Error("The href attribute of the 'styles*.css' file could not be found!");
+  }
+  return getURL(origin, filePath);
+};
+
+const getScriptPathFromHTML = (html: string, origin: string) => {
+  let regExp = /(?:|<script.*){1}src="(.*styles.*?\.js){1}"/g;
+  let filePath = "";
+  try {
+    filePath = getFirstCapturingGroup(regExp, html);
+  } catch {
+    throw new Error("The src attribute of the 'styles*.js' file could not be found!");
+  }
+  return getURL(origin, filePath);
+};
+
+const getFirstCapturingGroup = (regExp: RegExp, text: string) => {
+  let match = regExp.exec(text);
+  if (match && match[1]) {
+    return match[1];
+  } else {
+    throw new ReferenceError("No capture group found in the provided text.")
+  }
 }
 
 let generatedPdfBuffers: Array<Buffer> = [];
@@ -37,6 +66,9 @@ export async function generatePdf(
   const url = new URL(initialDocsUrl);
   const origin = url.origin;
 
+  let stylePath: string = "";
+  let scriptPath: string = "";
+
   let nextPageUrl = initialDocsUrl;
 
   while (nextPageUrl) {
@@ -44,8 +76,13 @@ export async function generatePdf(
     console.log(chalk.cyan(`Generating PDF of ${nextPageUrl}`));
     console.log();
 
-    await page.goto(`${nextPageUrl}`, {waitUntil: 'networkidle2'});
-      
+    await page.goto(`${nextPageUrl}`, { waitUntil: 'networkidle2' })
+      .then((resp) => resp?.text())
+      .then((html) => {
+        stylePath = getStylesheetPathFromHTML(html!, origin);
+        scriptPath = getScriptPathFromHTML(html!, origin);
+      });
+
     try {
       nextPageUrl = await page.$eval('.pagination-nav__item--next > a', (element) => {
         return (element as HTMLLinkElement).href;
@@ -53,17 +90,17 @@ export async function generatePdf(
     } catch (e) {
       nextPageUrl = "";
     }
-  
-  
+
+
     let html = await page.$eval('article', (element) => {
       return element.outerHTML;
     });
-  
-    
+
+
     await page.setContent(html);
-    await page.addStyleTag({url: safeUrlAppend(origin, baseUrl,'styles.css')});
-    await page.addScriptTag({url: safeUrlAppend(origin, baseUrl, 'styles.js')});
-    const pdfBuffer = await page.pdf({path: "", format: 'A4', printBackground: true, margin : {top: 25, right: 35, left: 35, bottom: 25}});
+    await page.addStyleTag({ url: stylePath });
+    await page.addScriptTag({ url: scriptPath });
+    const pdfBuffer = await page.pdf({ path: "", format: 'A4', printBackground: true, margin: { top: 25, right: 35, left: 35, bottom: 25 } });
 
     generatedPdfBuffers.push(pdfBuffer);
 
